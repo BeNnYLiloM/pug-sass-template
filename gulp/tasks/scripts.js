fa72e47f3505c234ad7638b45426eb6380e7535b
BeNnYLiloM <benny-krid@mail.ru>
2017-08-02 01:13:19 +0300

    First commit

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,8 +1,13 @@
 module.exports = function() {
+    let libs = [
+        'dev/libs/jquery/dist/jquery.min.js',
+        'dev/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+        'dev/libs/slick-carousel/slick/slick.min.js',
+        'dev/libs/svg4everybody/dist/svg4everybody.min.js',
+    ];
+
     $.gulp.task('libsJS:dev', () => {
-        return $.gulp.src([
-                'node_modules/svg4everybody/dist/svg4everybody.min.js',
-            ])
+        return $.gulp.src(libs)
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gulp.dest('./build/static/js/'))
             .pipe($.browserSync.reload({
@@ -11,9 +16,7 @@ module.exports = function() {
     });
 
     $.gulp.task('libsJS:build', () => {
-        return $.gulp.src([
-                'node_modules/svg4everybody/dist/svg4everybody.min.js',
-            ])
+        return $.gulp.src(libs)
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gp.uglifyjs())
             .pipe($.gulp.dest('./build/static/js/'));
